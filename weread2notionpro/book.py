99e323a503a81f587410aaf5324198c47c56d25d
Emma-Miao <51030558+Emma-Miao@users.noreply.github.com>
2025-01-30 15:19:35 +0000

    Only get the first author instead of all authors in case of wrong splitting.

--- weread2notionpro/book.py
+++ weread2notionpro/book.py
@@ -59,12 +59,9 @@ def insert_book_to_notion(books, index, bookId):
         book["ISBN"] = book.get("isbn")
         book["链接"] = weread_api.get_url(bookId)
         book["简介"] = book.get("intro")
-        book["作者"] = [
-            notion_helper.get_relation_id(
-                x, notion_helper.author_database_id, USER_ICON_URL
-            )
-            for x in book.get("author").split(" ")
-        ]
+        book["作者"] = notion_helper.get_relation_id(
+            book.get("author").split(" ")[0], notion_helper.author_database_id, USER_ICON_URL
+        )
         if book.get("categories"):
             book["分类"] = [
                 notion_helper.get_relation_id(
